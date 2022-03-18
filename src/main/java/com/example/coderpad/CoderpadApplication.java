aa59cd75006a82a367ee29406a8c376b48ad0f6e
chenyangwei95 <chenyangwei95@gmail.com>
2022-03-17 23:30:20 -0700

    Init CoderPad project

---
+++ src/main/java/com/example/coderpad/CoderpadApplication.java
@@ -0,0 +1,13 @@
+package com.example.coderpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coderpad/CoderpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coderpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
